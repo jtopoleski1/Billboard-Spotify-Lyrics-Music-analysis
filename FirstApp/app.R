556a922156b1de6c1a911efb91db984a4210193f
Jordan Topoleski <jordantopoleski@Jordans-MacBook-Pro-5.local>
2018-11-29 09:25:17 -0500

    added first shiny app

---
+++ FirstApp/app.R
@@ -0,0 +1,54 @@
+library(shiny)
+library(gganimate)
+library(dplyr)
+library(ggplot2)
+library(tidytext)
+library(wordcloud)
+library(tm)
+library(SnowballC)
+library(RColorBrewer)
+library(RCurl)
+library(XML)
+library(tidyverse)
+
+songs <- read_csv("songs.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Old Faithful Geyser Data"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
