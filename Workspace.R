193132a3e64659f43c3aaebac5a87f4f0d6d333f
Jordan Topoleski <jordantopoleski@Jordans-MacBook-Pro-5.local>
2018-12-13 15:24:15 -0500

    final commenting

--- Workspace.R
+++ Workspace.R
@@ -1,5 +1,7 @@
 # The purpose of this workspace is to save the csv file as an rds
 # to allow for easy incorperation into the shiny app
+# Previous to reading in the csv file, the data had been
+# cleaned extremely well, leading me to not have to do this.
 
 # Load the neccessary packages
 
